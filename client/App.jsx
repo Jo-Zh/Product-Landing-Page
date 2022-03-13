e42ba267f09299006c1f4d227caf7f01bfb7d97e
Jo-Zh <joannaweinazh@gmail.com>
2022-03-13 20:28:06 +1000

    index.css cannot add to index.html

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const App = () => {
-  return <h1>This is my Landing page in React</h1>;
+  return (
+    <div>
+      <img src="../img/apple-pie.png" alt="apple pie" />
+      <h1>This is my Landing page in React</h1>;
+    </div>
+  );
 };
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,21 +1,19 @@
 import React from "react";
 import { renderToString } from "react-dom/server";
 import express from "express";
-import { readFile, readFileSync } from "fs";
+import { readFileSync } from "fs";
 import App from "../client/App";
+import path from "path";
 
 const app = new express();
 const port = 3100;
 
+// app.use(express.static("public"));
 app.use(express.static("dist"));
 app.get("/", async (_req, res) => {
-  const style = readFile("public/index.css");
-  const index = readFileSync("public/index.html", "utf8");
+  const index = readFileSync(path.join("public", "/index.html"), "utf8");
   const rendered = renderToString(<App />);
-  res.send(style);
   res.send(index.replace("{{indicator}}", rendered)); //server know render the component to wanted place in html
-
-  // res.send("<h1>Product Landing Page</h1>");
 });
 
 app.listen(port);
